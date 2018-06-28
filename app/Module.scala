49203e3d9d1c33edc003ddd1b1542f87a89d9040
Amanda <amandagmsd@hotmail.com>
2018-06-28 15:30:20 -0300

    alteracoes amanda

--- app/Module.scala
+++ app/Module.scala
@@ -6,7 +6,7 @@ import services.{ ApplicationTimer, AtomicCounter, Counter }
 /**
  * This class is a Guice module that tells Guice how to bind several
  * different types. This Guice module is created when the Play
- * application starts.
+ * application srts.
  *
  * Play will automatically use any class called `Module` that is in
  * the root package. You can create modules in other locations by
